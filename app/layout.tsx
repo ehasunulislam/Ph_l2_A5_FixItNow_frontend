7f94ec85a92696af9e3485e5ae00cfaf4350741b
ehasun15019 <ehasunulislam0202@gmail.com>
2026-08-02 16:07:06 +0600

    updated vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode}>) {
   const user = await getCurrentUser();
 
-
   return (
   <html
     lang="en"
